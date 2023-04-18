e17eadc19e1cf4ac0516f004db8bb5e780fe604d
shuai.qin <shuai.qin@rjoy.com>
2023-04-18 10:50:57 +0800

    database

---
+++ HelloWorld/Assets/Scripts/SubScripts/Data/DataManager.cs
@@ -0,0 +1,22 @@
+using MainAssembly;
+
+namespace HotAssembly
+{
+    public class DataManager : Singletion<DataManager>
+    {
+        private TestData testData;
+
+        public TestData TestData { get => testData ??= new TestData(); }
+
+
+        public void Clear()
+        {
+            testData.Clear();
+            testData = null;
+        }
+    }
+    public interface Database
+    {
+        public void Clear();
+    }
+}
