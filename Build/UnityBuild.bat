bef4c849ab3308dcdc6a88432c68fdb5cdd6e1b2
shuai.qin <shuai.qin@rjoy.com>
2023-11-16 15:37:21 +0800

    打包分文件夹

--- Build/UnityBuild.bat
+++ Build/UnityBuild.bat
@@ -8,13 +8,16 @@ git reset --hard %1
 git pull
 
 taskkill /f /im Unity.exe
-echo 执行打包
+
+set path=D:\HelloWorld\Build\%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+if %2==BuildEditor.BuildPlayer (md %path%)
+
 "C:/Program Files/Unity2021.3.25/Editor/Unity.exe" ^
 -quit ^
 -batchmode ^
 -projectPath "D:/HelloWorld/HelloWorld" ^
 -logFile "D:/HelloWorld/Build/build.log" ^
 -executeMethod %2 ^
---path:D:\HelloWorld\Build\HelloWorld.apk ^
+--path:%path%\HelloWorld.apk ^
 --version:%3 ^
 --develop:%4
