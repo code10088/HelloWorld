b187e0ec24cd0cea46eaed954b16cc67c6a2dec7
shuai.qin <shuai.qin@rjoy.com>
2023-11-24 12:18:57 +0800

    CoroutineManager

--- HelloWorld/Assets/Scripts/MainScripts/CoroutineManager.cs
+++ HelloWorld/Assets/Scripts/MainScripts/CoroutineManager.cs
@@ -6,12 +6,12 @@ public class CoroutineManager : MonoSingleton<CoroutineManager>
 {
     private int uniqueId = 0;
     private Dictionary<int, Coroutine> coroutines = new Dictionary<int, Coroutine>();
-    public int StartCoroutinea(IEnumerator a)
+    public int Start(IEnumerator a)
     {
         coroutines[++uniqueId] = StartCoroutine(a);
         return uniqueId;
     }
-    public void StopCoroutinea(int id)
+    public void Stop(int id)
     {
         Coroutine c;
         if (coroutines.TryGetValue(id, out c))
