ecaa05465c0dd63c1aede70e727ea9f41bfe4bdf
shuai.qin <shuai.qin@rjoy.com>
2024-05-09 17:55:28 +0800

    bgr测试代码

--- HelloWorld/Assets/Scripts/SubScripts/Scene/Auto/TestSceneComponent.cs
+++ HelloWorld/Assets/Scripts/SubScripts/Scene/Auto/TestSceneComponent.cs
@@ -4,14 +4,12 @@ namespace HotAssembly
     public partial class TestSceneComponent
     {
         public GameObject obj;
-        public GameObject cubeObj = null;
         public UnityEngine.Transform fireRootTransform = null;
         public void Init(GameObject obj)
         {
             this.obj = obj;
             ExportComponent[] allData = obj.GetComponentsInChildren<ExportComponent>(true);
-            cubeObj = allData[0].gameObject;
-            fireRootTransform = allData[1].exportComponent[0] as UnityEngine.Transform;
+            fireRootTransform = allData[0].exportComponent[0] as UnityEngine.Transform;
         }
     }
 }
