7059ee625423ab4b624cd350a270bb48d5007a2c
shuai.qin <shuai.qin@rjoy.com>
2024-05-13 11:24:38 +0800

    简化

--- HelloWorld/Assets/Scripts/MainScripts/BRG/BRG_Background.cs
+++ HelloWorld/Assets/Scripts/MainScripts/BRG/BRG_Background.cs
@@ -3,7 +3,6 @@ using Unity.Mathematics;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
-using Unity.Profiling;
 using System.Collections.Generic;
 
 public unsafe class BRG_Background : MonoBehaviour
@@ -13,14 +12,8 @@ public unsafe class BRG_Background : MonoBehaviour
     public Mesh m_mesh;
     public Material m_material;
     public bool m_castShadows;
-    public float m_motionSpeed = 0f;
     public float m_motionAmplitude = 2.0f;
-    public float m_spacingFactor = 1.0f;
-    public bool m_debrisDebugTest = false;
     public float m_phaseSpeed1 = 6.0f;
-
-    static readonly ProfilerMarker s_BackgroundGPUSetData = new ProfilerMarker("BRG_Background.GPUSetData");
-    static readonly ProfilerMarker s_DebrisGPUSetData = new ProfilerMarker("BRG_Debris.GPUSetData");
 
     public int m_backgroundW = 60;
     public int m_backgroundH = 150;
@@ -29,11 +22,7 @@ public unsafe class BRG_Background : MonoBehaviour
     private BRG_Container m_brgContainer;
     private JobHandle m_updateJobFence;
 
-    private List<int> m_magnetCells = new List<int>();
-
     private int m_itemCount;
-    private float m_phase = 0.0f;
-    private float m_burstTimer = 0.0f;
     private uint m_slicePos;
 
     public struct BackgroundItem
@@ -42,9 +31,6 @@ public unsafe class BRG_Background : MonoBehaviour
         public float hInitial;
         public float h;     // scale
         public float phase;
-        public int weight;
-        public float magnetIntensity;
-        public float flashTime;
         public Vector4 color;
     };
 
@@ -53,37 +39,6 @@ public unsafe class BRG_Background : MonoBehaviour
     public void Awake()
     {
         gBackgroundManager = this;
-    }
-
-    private int cellIdCompute(Vector3 pos)
-    {
-        float smoothScrolling = m_phase;
-        // find background cell
-        uint xc = (uint)(int)(pos.x);
-        uint zc = (uint)(int)(pos.z + 0.5f + smoothScrolling);
-        if ((xc < m_backgroundW) && (zc < m_backgroundH))
-        {
-            zc = (m_slicePos + zc) % (uint)m_backgroundH;
-            return (int)(zc * m_backgroundW + xc);
-        }
-        else
-            return -1;
-    }
-
-    // if framerate drops, we may miss some cells if missile delta position is huge. So the while loop to trigger any in between cell
-    // ( most of the time the loop is a single iteration )
-    public void SetMagnetCell(Vector3 previousPos, Vector3 pos)
-    {
-        int cellStart = cellIdCompute(previousPos);
-        int cellEnd = cellIdCompute(pos);
-        if ((cellStart >= 0) && (cellEnd >= 0))
-        {
-            while (cellStart <= cellEnd)
-            {
-                m_magnetCells.Add(cellStart);
-                cellStart += m_backgroundW;
-            }
-        }
     }
 
     [BurstCompile]
@@ -106,18 +61,7 @@ public unsafe class BRG_Background : MonoBehaviour
                 scaleY += amp / (float)(x + 1);
             }
 
-            //            scaleY = ((m_slicePos&1)!=0) ? 1.0f : 2.0f;
-            //            scaleY = (((x^m_slicePos)&1)!=0) ? 1.0f : 2.0f;
-
-
-            float fy = scaleY * 0.5f;
-
-            //float sat = 0.6f + 0.2f + 0.2f * Mathf.Sin((m_slicePos + x) / 12.0f);
             float sat = 0.6f + 0.2f + 0.2f * Mathf.Sin((m_slicePos + x) / 12.0f);
-
-
-            //            float value = UnityEngine.Random.Range(0.0f, 1.0f);
-            //            float sat = 0.5f;
             float value = 0.3f;
 
             if ((x > 0) && (x < m_backgroundW - 1))
@@ -137,8 +81,6 @@ public unsafe class BRG_Background : MonoBehaviour
             item.hInitial = scaleY;
             item.phase = (float)x / 2.0f;
             item.color = new Vector4(col.r, col.g, col.b, 1.0f);
-            item.weight = 0;
-            item.flashTime = 0.0f;
             m_backgroundItems[sPos0] = item;
 
             sPos0++;
@@ -175,7 +117,6 @@ public unsafe class BRG_Background : MonoBehaviour
         [NativeDisableParallelForRestriction]
         public NativeArray<BackgroundItem> backgroundItems;
 
-        public float smoothScroll;
         public int slicePos;
         public int backgroundW;
         public int backgroundH;
@@ -187,7 +128,7 @@ public unsafe class BRG_Background : MonoBehaviour
         public void Execute(int sliceIndex)
         {
             int slice = (int)((sliceIndex + slicePos) % backgroundH);
-            float px = -smoothScroll + (float)sliceIndex - backgroundH / 2;
+            float px = (float)sliceIndex - backgroundH / 2;
             float phaseSpeed = _phaseSpeed * _dt;
 
 
@@ -197,28 +138,17 @@ public unsafe class BRG_Background : MonoBehaviour
                 BackgroundItem item = backgroundItems[itemId];
 
                 float4 color = backgroundItems[itemId].color;
-                if (item.flashTime > 0.0f)
-                {
-                    color = math.lerp(color, new float4(1, 1, 1, 1), item.flashTime);
-                }
 
                 float4 bpos;
                 float waveY = item.hInitial + 0.5f + math.sin(item.phase) * 0.5f;
                 float scaleY = waveY;
-                if (item.magnetIntensity > 0.0f)
-                {
-                    float alpha = Mathf.SmoothStep(0, 1, item.magnetIntensity);
-                    scaleY += alpha * 1.5f;
-                    color = math.lerp(color, color + new float4(1.0f, 0.3f, 0.3f, 0), alpha);
-                    item.magnetIntensity -= _dt * 3.0f;
-                }
 
                 bpos.z = item.z;
                 bpos.y = scaleY * 0.5f - 20;
 
                 item.h = scaleY;
 
-                float phaseInc = (item.weight <= 0) ? phaseSpeed : phaseSpeed * 0.3f;
+                float phaseInc = phaseSpeed;
                 item.phase += phaseInc;
 
                 int i;
@@ -234,7 +164,6 @@ public unsafe class BRG_Background : MonoBehaviour
                 _sysmemBuffer[(windowOffsetInFloat4 + _maxInstancePerWindow * 3 * 1 + i * 3 + 0)] = new float4(1, 0, 0, 0);
                 _sysmemBuffer[(windowOffsetInFloat4 + _maxInstancePerWindow * 3 * 1 + i * 3 + 1)] = new float4(1, 0, 0, 0);
                 _sysmemBuffer[(windowOffsetInFloat4 + _maxInstancePerWindow * 3 * 1 + i * 3 + 2)] = new float4(1, 0, 0, 0);
-                item.flashTime -= _dt * 1.0f;     // 1 second white flash
 
                 // update colors
                 _sysmemBuffer[windowOffsetInFloat4 + _maxInstancePerWindow * 3 * 2 + i] = color;
@@ -247,7 +176,7 @@ public unsafe class BRG_Background : MonoBehaviour
 
 
     [BurstCompile]
-    JobHandle UpdatePositions(float smoothScroll, float dt, JobHandle jobFence)
+    JobHandle UpdatePositions(float dt, JobHandle jobFence)
     {
         int totalGpuBufferSize;
         int alignedWindowSize;
@@ -257,7 +186,6 @@ public unsafe class BRG_Background : MonoBehaviour
         {
             _sysmemBuffer = sysmemBuffer,
             backgroundItems = m_backgroundItems,
-            smoothScroll = smoothScroll,
             slicePos = (int)m_slicePos,
             backgroundW = m_backgroundW,
             backgroundH = m_backgroundH,
@@ -274,60 +202,16 @@ public unsafe class BRG_Background : MonoBehaviour
     void Update()
     {
         float dt = Time.deltaTime;
-        m_phase += dt * m_motionSpeed;
-        m_burstTimer -= dt;
-
-        //while (m_phase >= 1.0f)
-        //{
-        //    InjectNewSlice();
-        //    m_phase -= 1.0f;
-        //}
-
-        if (Input.touchCount == 3)
-        {
-            if ((Input.touches[0].phase == TouchPhase.Began) &&
-                (Input.touches[1].phase == TouchPhase.Began) &&
-                (Input.touches[2].phase == TouchPhase.Began))
-                m_debrisDebugTest = !m_debrisDebugTest;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F7))
-            m_debrisDebugTest = !m_debrisDebugTest;
-
-        // read back the cell list with magnet and set intensity to 1
-        int magnetCellsCount = m_magnetCells.Count;
-        if (magnetCellsCount > 0)
-        {
-            for (int i = 0; i < magnetCellsCount; i++)
-            {
-                int cellId = m_magnetCells[i];
-                BackgroundItem item = m_backgroundItems[cellId];
-                item.magnetIntensity = 1.0f;
-                m_backgroundItems[cellId] = item;
-            }
-            m_magnetCells.Clear();
-        }
-
 
         JobHandle jobFence = new JobHandle();
 
-        m_updateJobFence = UpdatePositions(m_phase, dt, jobFence);
+        m_updateJobFence = UpdatePositions(dt, jobFence);
     }
 
     private void LateUpdate()
     {
-
         m_updateJobFence.Complete();
-
-        // upload ground cells
-        s_BackgroundGPUSetData.Begin();
         m_brgContainer.UploadGpuData(m_itemCount);
-        s_BackgroundGPUSetData.End();
-
-
-        // upload debris GPU data
-        s_DebrisGPUSetData.Begin();
-        s_DebrisGPUSetData.End();
     }
 
     private void OnDestroy()
