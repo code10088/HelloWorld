1d77f333b8c2d820115ab9448d7e1c19286517ba
shuai.qin <shuai.qin@rjoy.com>
2024-05-15 11:24:01 +0800

    PlayerPrefs

--- HelloWorld/Assets/Scripts/MainScripts/PlayerPrefs.cs
+++ HelloWorld/Assets/Scripts/MainScripts/PlayerPrefs.cs
@@ -4,47 +4,53 @@ public static class PlayerPrefs
 {
     public static void SetInt(string key, int value)
     {
-        UnityEngine.PlayerPrefs.SetInt(key, value);
-        UnityEngine.PlayerPrefs.Save();
 #if WeChatGame && !UNITY_EDITOR
         WX.StorageSetIntSync(key, value);
+#else
+        UnityEngine.PlayerPrefs.SetInt(key, value);
+        UnityEngine.PlayerPrefs.Save();
 #endif
     }
     public static int GetInt(string key, int defaultValue = 0)
     {
-        return UnityEngine.PlayerPrefs.GetInt(key, defaultValue);
 #if WeChatGame && !UNITY_EDITOR
         return WX.StorageGetIntSync(key, defaultValue);
+#else
+        return UnityEngine.PlayerPrefs.GetInt(key, defaultValue);
 #endif
     }
     public static void SetFloat(string key, float value)
     {
-        UnityEngine.PlayerPrefs.SetFloat(key, value);
-        UnityEngine.PlayerPrefs.Save();
 #if WeChatGame && !UNITY_EDITOR
         WX.StorageSetFloatSync(key,value);
+#else
+        UnityEngine.PlayerPrefs.SetFloat(key, value);
+        UnityEngine.PlayerPrefs.Save();
 #endif
     }
     public static float GetFloat(string key, float defaultValue = 0)
     {
-        return UnityEngine.PlayerPrefs.GetFloat(key, defaultValue);
 #if WeChatGame && !UNITY_EDITOR
         return WX.StorageGetFloatSync(key, defaultValue);
+#else
+        return UnityEngine.PlayerPrefs.GetFloat(key, defaultValue);
 #endif
     }
     public static void SetString(string key, string value)
     {
-        UnityEngine.PlayerPrefs.SetString(key, value);
-        UnityEngine.PlayerPrefs.Save();
 #if WeChatGame && !UNITY_EDITOR
         WX.StorageSetStringSync(key,value);
+#else
+        UnityEngine.PlayerPrefs.SetString(key, value);
+        UnityEngine.PlayerPrefs.Save();
 #endif
     }
     public static string GetString(string key, string defaultValue = "")
     {
-        return UnityEngine.PlayerPrefs.GetString(key, defaultValue);
 #if WeChatGame && !UNITY_EDITOR
         return WX.StorageGetStringSync(key,defaultValue);
+#else
+        return UnityEngine.PlayerPrefs.GetString(key, defaultValue);
 #endif
     }
 }
