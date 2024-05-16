950561bc94b24c243d51baa0d7164ead8a77156e
shuai.qin <shuai.qin@rjoy.com>
2024-05-16 16:28:38 +0800

    自动打包log问题

--- Build/Log.py
+++ Build/Log.py
@@ -5,5 +5,5 @@ import time
 log_file = 'D:/HelloWorld/Build/build.log'
 
 if os.path.exists(log_file):
-    with open(log_file, 'r', encoding='utf-8', errors='ignore') as file:
+    with open(log_file, 'r', encoding='gbk', errors='ignore') as file:
         print(file.read())
