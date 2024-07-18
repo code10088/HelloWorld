9fd8a529944a5594583a43cc06c0caab0f6ebec1
shuai.qin <shuai.qin@rjoy.com>
2024-07-18 18:05:02 +0800

    rvo

---
+++ HelloWorld/Assets/Tools/RVO/Interface/Agent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RVO;
+using UnityEngine;
+using Random = System.Random;
+using Vector2 = RVO.Vector2;
+
+public class Agent
+{
+    public int agentId = -1;
+    public Transform transform;
+    public Vector3 target;
+
+    private Random random = new Random();
+
+    public void Init(int agentId, Transform transform)
+    {
+        this.agentId = agentId;
+        this.transform = transform;
+    }
+    public void Clear()
+    {
+        Simulator.Instance.RemoveAgent(agentId);
+        agentId = -1;
+        transform = null;
+    }
+    public void RefreshTarget(Vector3 target)
+    {
+        this.target = target;
+    }
+    public void Update()
+    {
+        Vector2 pos = Simulator.Instance.getAgentPosition(agentId);
+        transform.position = new Vector3(pos.x(), pos.y());
+
+        Vector3 dir = target - transform.position;
+        Vector2 vector = new Vector2(dir.x, dir.y);
+        if (RVOMath.absSq(vector) > 1.0f) vector = RVOMath.normalize(vector);
+        transform.forward = dir;
+
+        Simulator.Instance.setAgentPrefVelocity(agentId, vector);
+        float angle = random.Next() * 2.0f * (float)Math.PI;
+        float dis = random.Next() * 0.0001f;
+        Simulator.Instance.setAgentPrefVelocity(agentId, vector + dis * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)));
+    }
+}
---
+++ HelloWorld/Assets/Tools/RVO/Interface/RVOManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using RVO;
+using UnityEngine;
+using Vector2 = RVO.Vector2;
+
+public class RVOManager : Singletion<RVOManager>
+{
+    private int updateId = -1;
+
+    private List<Agent> agents = new List<Agent>();
+    private Queue<Agent> cache = new Queue<Agent>();
+
+    public void Init()
+    {
+        Simulator.Instance.setTimeStep(0.25f);
+        Simulator.Instance.setAgentDefaults(15.0f, 10, 5.0f, 5.0f, 2.0f, 2.0f, new Vector2(0.0f, 0.0f));
+        Simulator.Instance.SetNumWorkers(1);
+    }
+
+    public void Start()
+    {
+        if (updateId < 0) updateId = Updater.Instance.StartUpdate(Update);
+    }
+    private void Update()
+    {
+        Simulator.Instance.doStep();
+        for (int i = 0; i < agents.Count; i++) agents[i].Update();
+    }
+    public void Stop()
+    {
+        if (updateId > 0) Updater.Instance.StopUpdate(updateId);
+    }
+    public void Clear()
+    {
+        Simulator.Instance.Clear();
+        agents.Clear();
+    }
+
+    public void AddObstacle(GameObject obj)
+    {
+        BoxCollider2D[] colliders = obj.GetComponentsInChildren<BoxCollider2D>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            var pos = colliders[i].transform.position;
+            var scale = colliders[i].transform.localScale;
+            var size = colliders[i].size;
+            float minX = pos.x - size.x * scale.x * 0.5f;
+            float minY = pos.y - size.y * scale.y * 0.5f;
+            float maxX = pos.x + size.x * scale.x * 0.5f;
+            float maxY = pos.y + size.y * scale.y * 0.5f;
+            IList<Vector2> obstacle = new List<Vector2>()
+            {
+                new Vector2(minX, minY),
+                new Vector2(maxX, minY),
+                new Vector2(maxX, maxY),
+                new Vector2(minX, maxY),
+            };
+            Simulator.Instance.addObstacle(obstacle);
+        }
+        Simulator.Instance.processObstacles();
+    }
+    public Agent AddAgent(Vector2 pos, Transform transform)
+    {
+        int agentId = Simulator.Instance.addAgent(pos);
+        if (agentId < 0) return null;
+        Agent agent = null;
+        if (cache.Count > 0) agent = cache.Dequeue();
+        else agent = new Agent();
+        agent.Init(agentId, transform);
+        agents.Add(agent);
+        return agent;
+    }
+    public void RemoveAgent(int agentId)
+    {
+        int index = agents.FindIndex(a => a.agentId == agentId);
+        var agent = agents[index];
+        agent.Clear();
+        cache.Enqueue(agent);
+        agents.RemoveAt(index);
+    }
+}
