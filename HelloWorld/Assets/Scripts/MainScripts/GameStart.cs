614be48c973e5b1825cceed17219e4b79457e5e0
shuai.qin <shuai.qin@rjoy.com>
2024-07-26 10:49:46 +0800

    pdb报错

--- HelloWorld/Assets/Scripts/MainScripts/GameStart.cs
+++ HelloWorld/Assets/Scripts/MainScripts/GameStart.cs
@@ -85,11 +85,7 @@ public class GameStart : MonoSingletion<GameStart>
     {
         TextAsset dll = asset[0] as TextAsset;
         TextAsset pdb = asset[1] as TextAsset;
-#if UNITY_WEBGL
         var hotAssembly = Assembly.Load(dll.bytes);
-#else
-        var hotAssembly = Assembly.Load(dll.bytes, pdb.bytes);
-#endif
         AssetManager.Instance.Unload(ref loadId);
         Type t = hotAssembly.GetType("HotAssembly.GameStart");
         PropertyInfo p = t.BaseType.GetProperty("Instance");
