8ae116d073211bbf01a9acdcb04e168c21945f44
qinshuai <13140167660@163.com>
2024-08-15 12:55:09 +0800

    优化asyncmanager

--- HelloWorld/Assets/Scripts/MainScripts/AsyncManager.cs
+++ HelloWorld/Assets/Scripts/MainScripts/AsyncManager.cs
@@ -1,40 +1,78 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 using YooAsset;
 
 public class AsyncManager : Singletion<AsyncManager>
 {
-    private List<AsyncItem> list1 = new(50);//不切片列表
-    private List<AsyncItem> list2 = new(1000);//切片列表
+    private AsyncItem[] array1 = new AsyncItem[100];//不切片列表
+    private AsyncItem[] array2 = new AsyncItem[10000];//切片列表
+    private byte[] mark1 = new byte[100];
+    private byte[] mark2 = new byte[10000];
+    private int count1 = 0;
+    private int count2 = 0;
     private float gcTimer = 0;
     private int idxMark = 0;
 
     public void Add(AsyncItem item, bool ignoreFrameTime)
     {
-        (ignoreFrameTime ? list1 : list2).Add(item);
+        if (ignoreFrameTime)
+        {
+            for (int i = 0; i < count1; i++)
+            {
+                if (mark1[i] == 0)
+                {
+                    array1[i] = item;
+                    mark1[i] = 1;
+                    return;
+                }
+            }
+            array1[count1] = item;
+            mark1[count1] = 1;
+            count1++;
+        }
+        else
+        {
+            for (int i = 0; i < count2; i++)
+            {
+                if (mark2[i] == 0)
+                {
+                    array2[i] = item;
+                    mark2[i] = 1;
+                    return;
+                }
+            }
+            array2[count2] = item;
+            mark2[count2] = 1;
+            count2++;
+        }
     }
     public void Remove(int id, bool execMark)
     {
         AsyncItem temp;
-        for (int i = 0; i < list1.Count; i++)
+        for (int i = 0; i < count1; i++)
         {
-            temp = list1[i];
-            if (temp.ItemID == id)
+            if (mark1[i] > 0)
             {
-                temp.endMark = true;
-                temp.execMark = execMark;
-                return;
+                temp = array1[i];
+                if (temp.ItemID == id)
+                {
+                    temp.endMark = true;
+                    temp.execMark = execMark;
+                    return;
+                }
             }
         }
-        for (int i = 0; i < list2.Count; i++)
+        for (int i = 0; i < count2; i++)
         {
-            temp = list2[i];
-            if (temp.ItemID == id)
+            if (mark2[i] > 0)
             {
-                temp.endMark = true;
-                temp.execMark = execMark;
-                return;
+                temp = array2[i];
+                if (temp.ItemID == id)
+                {
+                    temp.endMark = true;
+                    temp.execMark = execMark;
+                    return;
+                }
             }
         }
     }
@@ -43,49 +81,74 @@ public class AsyncManager : Singletion<AsyncManager>
         float t = Time.realtimeSinceStartup;
         YooAssets.SetOperationSystemFrameTime(t);
         AsyncItem temp;
-        for (int i = 0; i < list1.Count; i++)
+        for (int i = 0; i < count1; i++)
         {
-            temp = list1[i];
-            temp.Update();
-            if (temp.endMark)
+            if (mark1[i] > 0)
             {
-                temp.Finish();
-                list1.RemoveAt(i);
-                temp.Reset();
-                i--;
+                temp = array1[i];
+                temp.Update();
+                if (temp.endMark)
+                {
+                    temp.Finish();
+                    temp.Reset();
+                    mark1[i] = 0;
+                }
             }
         }
-        for (int i = 0; i < list2.Count; i++)
+        for (int i = 0; i < count2; i++)
         {
-            int idx = (i + idxMark) % list2.Count;
-            temp = list2[idx];
-            temp.Update();
-            if (temp.endMark)
+            int idx = (i + idxMark) % count2;
+            if (mark2[idx] > 0)
             {
-                temp.Finish();
-                list2.RemoveAt(idx);
-                temp.Reset();
-                idx--;
-                i--;
+                temp = array2[idx];
+                temp.Update();
+                if (temp.endMark)
+                {
+                    temp.Finish();
+                    temp.Reset();
+                    mark2[idx] = 0;
+                }
             }
             if (Time.realtimeSinceStartup - t > GameSetting.updateTimeSliceS)
             {
-                //list2.Count=9 ids最大为8
-                //ids=0
-                //  无删除(0+1)%9=1
-                //  有删除(0-1+1)%8=0
-                //ids=7
-                //  无删除(7+1)%9=8
-                //  有删除(7-1+1)%8=7
-                //ids=8
-                //  无删除(8+1)%9=0
-                //  有删除(8-1+1)%8=0
-                idxMark = (idx + 1) % Math.Max(1, list2.Count);
+                idxMark = (idx + 1) % Math.Max(1, count2);
                 return;
             }
         }
         if (Time.realtimeSinceStartup - gcTimer > GameSetting.gcTimeIntervalS)
         {
+            int offset = 0;
+            for (int i = 0; i < count1; i++)
+            {
+                int idx = i - offset;
+                if (mark1[i] == 0) offset++;
+                array1[idx] = array1[i];
+                mark1[idx] = 1;
+            }
+            for (int i = 0; i < offset; i++)
+            {
+                int idx = count1 - 1 - i;
+                array1[idx] = null;
+                mark1[idx] = 0;
+            }
+            count1 -= offset;
+
+            offset = 0;
+            for (int i = 0; i < count2; i++)
+            {
+                int idx = i - offset;
+                if (mark2[i] == 0) offset++;
+                array2[idx] = array2[i];
+                mark2[idx] = 1;
+            }
+            for (int i = 0; i < offset; i++)
+            {
+                int idx = count2 - 1 - i;
+                array2[idx] = null;
+                mark2[idx] = 0;
+            }
+            count2 -= offset;
+
             gcTimer = Time.realtimeSinceStartup;
             GC.Collect();
         }
