97bfcff417a95282d863de3ddcef8024f8b52f03
qinshuai <13140167660@163.com>
2025-02-28 20:37:10 +0800

    更新yooasset

--- HelloWorld/Assets/YooAsset/Editor/AssetBundleDebugger/VisualViewers/DebuggerOperationListViewer.cs
+++ HelloWorld/Assets/YooAsset/Editor/AssetBundleDebugger/VisualViewers/DebuggerOperationListViewer.cs
@@ -21,8 +21,12 @@ namespace YooAsset.Editor
         private TemplateContainer _root;
 
         private TableView _operationTableView;
+        private Toolbar _bottomToolbar;
+#if UNITY_2022_3_OR_NEWER
+        private TreeView _childTreeView;
+#endif
 
-        private DebugReport _debugReport;
+        private int _treeItemID = 0;
         private List<ITableData> _sourceDatas;
 
 
@@ -41,18 +45,34 @@ namespace YooAsset.Editor
 
             // 任务列表
             _operationTableView = _root.Q<TableView>("TopTableView");
+            _operationTableView.SelectionChangedEvent = OnOperationTableViewSelectionChanged;
             CreateOperationTableViewColumns();
+
+            // 底部标题栏
+            _bottomToolbar = _root.Q<Toolbar>("BottomToolbar");
+            CreateBottomToolbarHeaders();
+
+            // 子列表
+#if UNITY_2022_3_OR_NEWER
+            _childTreeView = _root.Q<TreeView>("BottomTreeView");
+            _childTreeView.makeItem = MakeTreeViewItem;
+            _childTreeView.bindItem = BindTreeViewItem;
+#endif
+
+            // 面板分屏
+            var topGroup = _root.Q<VisualElement>("TopGroup");
+            var bottomGroup = _root.Q<VisualElement>("BottomGroup");
+            topGroup.style.minHeight = 100;
+            bottomGroup.style.minHeight = 100f;
+            UIElementsTools.SplitVerticalPanel(_root, topGroup, bottomGroup);
         }
         private void CreateOperationTableViewColumns()
         {
-
             // PackageName
             {
                 var columnStyle = new ColumnStyle(200);
-                columnStyle.Stretchable = true;
                 columnStyle.Searchable = true;
                 columnStyle.Sortable = true;
-                columnStyle.Counter = true;
                 var column = new TableColumn("PackageName", "Package Name", columnStyle);
                 column.MakeCell = () =>
                 {
@@ -159,7 +179,7 @@ namespace YooAsset.Editor
                 columnStyle.Stretchable = false;
                 columnStyle.Searchable = false;
                 columnStyle.Sortable = true;
-                var column = new TableColumn("LoadingTime", "Loading Time", columnStyle);
+                var column = new TableColumn("ProcessTime", "Process Time", columnStyle);
                 column.MakeCell = () =>
                 {
                     var label = new Label();
@@ -202,6 +222,82 @@ namespace YooAsset.Editor
                 };
                 _operationTableView.AddColumn(column);
             }
+
+            // Desc
+            {
+                var columnStyle = new ColumnStyle(500, 500, 1000);
+                columnStyle.Stretchable = true;
+                columnStyle.Searchable = true;
+                var column = new TableColumn("Desc", "Desc", columnStyle);
+                column.MakeCell = () =>
+                {
+                    var label = new Label();
+                    label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                    return label;
+                };
+                column.BindCell = (VisualElement element, ITableData data, ITableCell cell) =>
+                {
+                    var infoLabel = element as Label;
+                    infoLabel.text = (string)cell.GetDisplayObject();
+                };
+                _operationTableView.AddColumn(column);
+            }
+        }
+        private void CreateBottomToolbarHeaders()
+        {
+            // OperationName
+            {
+                ToolbarButton button = new ToolbarButton();
+                button.text = "OperationName";
+                button.style.flexGrow = 0;
+                button.style.width = 315;
+                _bottomToolbar.Add(button);
+            }
+
+            // Progress
+            {
+                ToolbarButton button = new ToolbarButton();
+                button.text = "Progress";
+                button.style.flexGrow = 0;
+                button.style.width = 100;
+                _bottomToolbar.Add(button);
+            }
+
+            // BeginTime
+            {
+                ToolbarButton button = new ToolbarButton();
+                button.text = "BeginTime";
+                button.style.flexGrow = 0;
+                button.style.width = 100;
+                _bottomToolbar.Add(button);
+            }
+
+            // ProcessTime
+            {
+                ToolbarButton button = new ToolbarButton();
+                button.text = "ProcessTime";
+                button.style.flexGrow = 0;
+                button.style.width = 100;
+                _bottomToolbar.Add(button);
+            }
+
+            // Status
+            {
+                ToolbarButton button = new ToolbarButton();
+                button.text = "Status";
+                button.style.flexGrow = 0;
+                button.style.width = 100;
+                _bottomToolbar.Add(button);
+            }
+
+            // Desc
+            {
+                ToolbarButton button = new ToolbarButton();
+                button.text = "Desc";
+                button.style.flexGrow = 0;
+                button.style.width = 500;
+                _bottomToolbar.Add(button);
+            }
         }
 
         /// <summary>
@@ -209,10 +305,13 @@ namespace YooAsset.Editor
         /// </summary>
         public void FillViewData(DebugReport debugReport)
         {
-            _debugReport = debugReport;
-
             // 清空旧数据
             _operationTableView.ClearAll(false, true);
+
+#if UNITY_2022_3_OR_NEWER
+            _childTreeView.SetRootItems(new List<TreeViewItemData<DebugOperationInfo>>());
+            _childTreeView.Rebuild();
+#endif
 
             // 填充数据源
             _sourceDatas = new List<ITableData>(1000);
@@ -230,6 +329,7 @@ namespace YooAsset.Editor
                     rowData.AddStringValueCell("BeginTime", operationInfo.BeginTime);
                     rowData.AddLongValueCell("LoadingTime", operationInfo.ProcessTime);
                     rowData.AddStringValueCell("Status", operationInfo.Status.ToString());
+                    rowData.AddStringValueCell("Desc", operationInfo.OperationDesc);
                     _sourceDatas.Add(rowData);
                 }
             }
@@ -244,8 +344,11 @@ namespace YooAsset.Editor
         /// </summary>
         public void ClearView()
         {
-            _debugReport = null;
             _operationTableView.ClearAll(false, true);
+#if UNITY_2022_3_OR_NEWER
+            _childTreeView.SetRootItems(new List<TreeViewItemData<DebugOperationInfo>>());
+            _childTreeView.Rebuild();
+#endif
             RebuildView(null);
         }
 
@@ -276,6 +379,149 @@ namespace YooAsset.Editor
         {
             _root.RemoveFromHierarchy();
         }
+
+#if UNITY_2022_3_OR_NEWER
+        private void OnOperationTableViewSelectionChanged(ITableData data)
+        {
+            var operationTableData = data as OperationTableData;
+            DebugPackageData packageData = operationTableData.PackageData;
+            DebugOperationInfo operationInfo = operationTableData.OperationInfo;
+
+            _treeItemID = 0;
+            var rootItems = CreateTreeData(operationInfo);
+            _childTreeView.SetRootItems(rootItems);
+            _childTreeView.Rebuild();
+        }
+        private VisualElement MakeTreeViewItem()
+        {
+            VisualElement treeViewElement = new VisualElement();
+            treeViewElement.style.flexDirection = FlexDirection.Row;
+
+            // OperationName
+            {
+                Label label = new Label();
+                label.name = "OperationName";
+                label.style.flexGrow = 0f;
+                label.style.width = 300;
+                label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                treeViewElement.Add(label);
+            }
+
+            // Progress
+            {
+                var label = new Label();
+                label.name = "Progress";
+                label.style.flexGrow = 0f;
+                label.style.width = 100;
+                label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                treeViewElement.Add(label);
+            }
+
+            // BeginTime
+            {
+                var label = new Label();
+                label.name = "BeginTime";
+                label.style.flexGrow = 0f;
+                label.style.width = 100;
+                label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                treeViewElement.Add(label);
+            }
+
+            // ProcessTime
+            {
+                var label = new Label();
+                label.name = "ProcessTime";
+                label.style.flexGrow = 0f;
+                label.style.width = 100;
+                label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                treeViewElement.Add(label);
+            }
+
+            // Status
+            {
+                var label = new Label();
+                label.name = "Status";
+                label.style.flexGrow = 0f;
+                label.style.width = 100;
+                label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                treeViewElement.Add(label);
+            }
+
+            // Desc
+            {
+                Label label = new Label();
+                label.name = "Desc";
+                label.style.flexGrow = 1f;
+                label.style.width = 500;
+                label.style.unityTextAlign = TextAnchor.MiddleLeft;
+                treeViewElement.Add(label);
+            }
+
+            return treeViewElement;
+        }
+        private void BindTreeViewItem(VisualElement element, int index)
+        {
+            var operationInfo = _childTreeView.GetItemDataForIndex<DebugOperationInfo>(index);
+
+            // OperationName
+            {
+                var label = element.Q<Label>("OperationName");
+                label.text = operationInfo.OperationName;
+            }
+
+            // Progress
+            {
+                var label = element.Q<Label>("Progress");
+                label.text = operationInfo.Progress.ToString();
+            }
+
+            // BeginTime
+            {
+                var label = element.Q<Label>("BeginTime");
+                label.text = operationInfo.BeginTime;
+            }
+
+            // ProcessTime
+            {
+                var label = element.Q<Label>("ProcessTime");
+                label.text = operationInfo.ProcessTime.ToString();
+            }
+
+            // Status
+            {
+                StyleColor textColor;
+                if (operationInfo.Status == EOperationStatus.Failed.ToString())
+                    textColor = new StyleColor(Color.yellow);
+                else
+                    textColor = new StyleColor(Color.white);
+
+                var label = element.Q<Label>("Status");
+                label.text = operationInfo.Status;
+                label.style.color = textColor;
+            }
+
+            // Desc
+            {
+                var label = element.Q<Label>("Desc");
+                label.text = operationInfo.OperationDesc;
+            }
+        }
+        private List<TreeViewItemData<DebugOperationInfo>> CreateTreeData(DebugOperationInfo parentOperation)
+        {
+            var rootItemList = new List<TreeViewItemData<DebugOperationInfo>>();
+            foreach (var childOperation in parentOperation.Childs)
+            {
+                var childItemList = CreateTreeData(childOperation);
+                var treeItem = new TreeViewItemData<DebugOperationInfo>(_treeItemID++, childOperation, childItemList);
+                rootItemList.Add(treeItem);
+            }
+            return rootItemList;
+        }
+#else
+        private void OnOperationTableViewSelectionChanged(ITableData data)
+        {
+        }
+#endif
     }
 }
 #endif
