0f613fbe34560b03c8de2fbc98162461e35d5622
qinshuai <13140167660@163.com>
2025-04-19 10:56:08 +0800

    使用相对路径

--- NetMsg/Net/gen.bat
+++ NetMsg/Net/gen.bat
@@ -2,9 +2,9 @@
 setlocal enabledelayedexpansion
 chcp 65001 > nul
 
-set "PROTOGEN_PATH=D:\HelloWorld\NetMsg\Net\protogen.exe"
-set "PROTO_PATH=D:\HelloWorld\NetMsg\Proto"
-set "OUTCODES_PATH=D:\HelloWorld\NetMsg\Net\OutCodes"
+set "PROTOGEN_PATH=.\protogen.exe"
+set "PROTO_PATH=..\Proto"
+set "OUTCODES_PATH=.\OutCodes"
 
 rmdir /s /q %OUTCODES_PATH%
 mkdir %OUTCODES_PATH%
