9d4f64ceda81896749c395e293009b674f5f975c
qinshuai <13140167660@163.com>
2025-04-19 10:52:56 +0800

    消息

--- NetMsg/Net/gen.bat
+++ NetMsg/Net/gen.bat
@@ -4,11 +4,14 @@ chcp 65001 > nul
 
 set "PROTOGEN_PATH=D:\HelloWorld\NetMsg\Net\protogen.exe"
 set "PROTO_PATH=D:\HelloWorld\NetMsg\Proto"
+set "OUTCODES_PATH=D:\HelloWorld\NetMsg\Net\OutCodes"
 
-set count=0
+rmdir /s /q %OUTCODES_PATH%
+mkdir %OUTCODES_PATH%
+
 for /r %PROTO_PATH% %%f in (*.proto) do (
 	echo [处理] %%f
-    %PROTOGEN_PATH% %%~nxf --proto_path=%%~pf --csharp_out=OutCodes
+    %PROTOGEN_PATH% %%~nxf --proto_path=%%~pf --csharp_out=%OUTCODES_PATH%
 )
 
 echo [完成]
