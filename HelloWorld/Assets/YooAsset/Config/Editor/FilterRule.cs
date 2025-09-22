0281a55af7679fd5e38d1a65b18b1cc8a3abaed7
qinshuai <13140167660@163.com>
2025-09-22 16:00:07 +0800

    yooasset

--- HelloWorld/Assets/YooAsset/Config/Editor/FilterRule.cs
+++ HelloWorld/Assets/YooAsset/Config/Editor/FilterRule.cs
@@ -3,6 +3,8 @@ namespace YooAsset.Editor
     [DisplayName("收集UserData所有资源")]
     public class CollectUserData : IFilterRule
     {
+        public string FindAssetType => EAssetSearchType.All.ToString();
+
         public bool IsCollectAsset(FilterRuleData data)
         {
             string[] strs = data.UserData.Split(';', System.StringSplitOptions.RemoveEmptyEntries);
