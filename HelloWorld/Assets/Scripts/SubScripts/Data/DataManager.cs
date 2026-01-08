a87ff4807dcbe3fcd4c042c089be0e17028aa9c8
qinshuai <13140167660@163.com>
2026-01-08 11:26:49 +0800

    数据

--- HelloWorld/Assets/Scripts/SubScripts/Data/DataManager.cs
+++ HelloWorld/Assets/Scripts/SubScripts/Data/DataManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public interface DataBase
 {
     void Init();
@@ -5,5 +8,28 @@ public interface DataBase
 }
 public partial class DataManager : Singletion<DataManager>
 {
+    private Dictionary<Type, DataBase> dataMap = new Dictionary<Type, DataBase>();
 
+    [Obsolete("DataManager_Auto中自动生成相关Data")]
+    public T GetData<T>() where T : class, DataBase, new()
+    {
+        Type type = typeof(T);
+        if (dataMap.TryGetValue(type, out var data1))
+        {
+            return data1 as T;
+        }
+        else
+        {
+            T data2 = new T();
+            data2.Init();
+            dataMap[type] = data2;
+            return data2;
+        }
+    }
+    [Obsolete("DataManager_Auto中自动生成相关Data")]
+    public void ClearDataMap()
+    {
+        foreach (var data in dataMap.Values) data.Clear();
+        dataMap.Clear();
+    }
 }
